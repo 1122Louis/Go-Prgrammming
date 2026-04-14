243f5aab398003e7a55e10991f8a7607d32d1462
Louis <agbedorlouisdzidzor@Louis.local>
2026-04-15 00:34:23 +0200

    For Loop updated

--- Go-Prgrammming/Go-learning/For-loop/main.go
+++ Go-Prgrammming/Go-learning/For-loop/main.go
@@ -1,32 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	i := 1
-	for i <= 3 {
+	/*for i := 0; i <= 10; i++ {
 		fmt.Println(i)
-		i = i + 1
 	}
 
-	for j := 0; j < 3; j++ {
-		fmt.Println(j)
+	fmt.Println("------------------------")
+
+	m := []int{1, 2, 3, 4, 5}
+
+	for i, v := range m {
+		fmt.Println(i, v)
 	}
 
-	for i := range 3 {
-		fmt.Println("range", i)
+	fmt.Println("------------------------")
+
+	for i, v := range "Hello" {
+		fmt.Println(i, v)
 	}
 
-	for {
-		fmt.Println("loop")
-		break
+	fmt.Println("------------------------")
+
+	for i := 0; i < len(m); i++ {
+		fmt.Println(i, m[i])
 	}
 
-	for n := range 6 {
+	for _, v := range m {
+		fmt.Println(v)
+	}*/
+
+	// Print multiplication table for 7
+	table := 7
+	for i := 1; i <= 10; i++ {
+		m := i * table
+		fmt.Printf("%d x %d = %d\n", i, table, m)
+	}
+
+	// Print even numbers from 0 to 20
+	for n := 0; n <= 20; n++ {
 		if n%2 == 0 {
-			continue
+			fmt.Println(n, "is even")
 		}
-		fmt.Println(n)
+	}
+
+	// Print odd numbers from 0 to 20
+	for n := 0; n <= 20; n++ {
+		if n%2 != 0 {
+			fmt.Println(n, "is odd")
+		}
+	}
+
+	fmt.Println("----------------------------")
+
+	names := []string{"Alice", "Bob", "Charlie", "Diana"}
+
+	for i, name := range names {
+		if name == "Charlie" {
+			fmt.Println(name, "Found Charlie at index", i)
+			break
+		}
+	}
+
+	for i := 0; i <= 20; i++ {
+		if i%3 == 0 {
+			fmt.Println("Fix")
+
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FixBuzz")
+		} else {
+			fmt.Println(i)
+		}
 	}
 }
